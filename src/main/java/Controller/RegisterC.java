2fc8754f3ab3dec036a8a1c663891283ebd3f7b6
whbanner <china_wh@163.com>
2019-09-14 23:35:06 +0800

    register login

--- src/main/java/Controller/RegisterC.java
+++ src/main/java/Controller/RegisterC.java
@@ -1,5 +1,23 @@
 package Controller;
 
-public class RegisterC {
+import com.alibaba.fastjson.JSON;
+import pojo.Register;
 
+public class RegisterC {
+    public String data;
+    public String json;
+
+    public RegisterC() {
+    }
+
+    public RegisterC(String data) {
+        this.data = data;
+    }
+    public String Return(String data){
+        Register register = JSON.parseObject(data,Register.class);
+        int phone = register.getPhone();
+        String pwd =register.getPwd();
+        String verificationCode =register.getVerificationCode();
+        return data;
+    }
 }
